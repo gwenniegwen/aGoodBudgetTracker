d85464db020c30cac9e49653241bf41ae6ebc14f
Gwendolyn Thomas <gwenthomas85@gmail.com>
2020-05-12 17:45:58 -0700

    updates

---
+++ public/indexdb.js
@@ -0,0 +1,54 @@
+
+
+const request = window.indexedDB.open("budget", 1);
+let db;
+
+
+request.onupgradeneeded = function (e) {
+    const db = e.target.result;
+    db.createObjectStore("pending", { autoIncrement: true });
+};
+
+request.onerror = function (e) {
+    console.log("There was an error");
+};
+
+request.onsuccess = function (e) {
+    db = e.target.result;
+
+
+    if (navigator.onLine) {
+        checkDatabase();
+    }
+
+};
+function saveRecord(record){
+    const transaction = db.transaction(["pending"], "readwrite")
+    const store = transaction.objectStore("pending")
+    store.add(record)
+}
+function checkDatabase() {
+    const transaction = db.transaction(["pending"], "readwrite")
+    const store = transaction.objectStore("pending")
+    const getAll = store.getAll()
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(getAll.result)
+            }).then((res) => {
+                return res.json();
+
+            }).then(() => {
+                const transaction = db.transaction(["pending"], "readwrite")
+                const store = transaction.objectStore("pending")
+                store.clear()
+            })
+        }
+    }
+}
+window.addEventListener("online", checkDatabase)
